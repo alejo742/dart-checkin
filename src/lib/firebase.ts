7a1979c8243c7ce3fd8a04370f613177f0c6f456
Alejandro Manrique <alejandromanriquenunez@gmail.com>
2025-06-03 14:12:09 -0400

    add: full project layout and landing

---
+++ src/lib/firebase.ts
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD12F423dlaWX6v54th5ck952HgNeFMcuo",
+  authDomain: "dart-checkin.firebaseapp.com",
+  projectId: "dart-checkin",
+  storageBucket: "dart-checkin.firebasestorage.app",
+  messagingSenderId: "740770799500",
+  appId: "1:740770799500:web:4e22e9e9d5b322578cc0a9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
